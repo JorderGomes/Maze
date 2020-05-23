62aa7e38f8a7137f94a023b9557a783e3b270dcc
JorderGomes <jordergomes@hotmail.com>
2020-05-23 18:34:04 -0300

    vizinhos furaveis

--- js/menu.js
+++ js/menu.js
@@ -11,18 +11,18 @@ const embusca = 'embusca';
 const caminho = 'caminho';
 const abatido = 'abatido';
 
-var pilha = [], labrintoCompleto = [];
+var pilha = [], labrintoCompleto = [], tabuleiroCompleto = [];
 
 function gerarId(x, y){
     return "casa-" + x + "-" + y;
 }
 
-const Jogador = {
-    construtor(casa, x, y){
+class House  {
+    constructor(casa, x, y){
         this.casa = casa;
         this.x = x;
-        this. y = y;
-    },
+        this.y = y;
+    }
 
     atualizarCasa(novoX, novoY){
         this.x = novoX;
@@ -38,36 +38,43 @@ const Jogador = {
     }
 }
 
+
+const Jogador = {
+    construtor(house){
+        this.house = house;
+    }
+}
+
 const eventosPlayer = {
     ArrowUp(){
-        if(Jogador.x == 0){
+        if(Jogador.house.x == 0){
             return;
         }
-        Jogador.atualizarCasa(Jogador.x - 1, Jogador.y);
+        Jogador.house.atualizarCasa(Jogador.house.x - 1, Jogador.house.y);
         console.log("Andando para cima");
     },
 
     ArrowDown(){
-        if(Jogador.x == altura - 1){
+        if(Jogador.house.x == altura - 1){
             return;
         }
-        Jogador.atualizarCasa(Jogador.x + 1, Jogador.y);
+        Jogador.house.atualizarCasa(Jogador.house.x + 1, Jogador.house.y);
         console.log("Andando para baixo");
     },
 
     ArrowLeft(){
-        if(Jogador.y == 0){
+        if(Jogador.house.y == 0){
             return;
         }
-        Jogador.atualizarCasa(Jogador.x, Jogador.y - 1);
+        Jogador.house.atualizarCasa(Jogador.house.x, Jogador.house.y - 1);
         console.log("Andando para esquerda");
     },
 
     ArrowRight(){
-        if(Jogador.y == largura - 1){
+        if(Jogador.house.y == largura - 1){
             return;
         }
-        Jogador.atualizarCasa(Jogador.x, Jogador.y + 1);
+        Jogador.house.atualizarCasa(Jogador.house.x, Jogador.house.y + 1);
         console.log("Andando para direita");
     }
 }
@@ -78,29 +85,35 @@ function montarTabuleiro(){
         for(j = 0; j < altura; j++){
         let idCasa = gerarId(i, j);
         let casa = document.createElement("div");
+
         casa.classList.add('casa');
         casa.classList.add('vazia');
+        
         casa.id = idCasa;
+        
+        let house = new House(casa, i, j);
+
         tabuleiro.appendChild(casa);
+        tabuleiroCompleto.push(house);
         }
     }
-    atribuirCasaInicial();
+    gerarLabirinto();
+    // escolherCasaAleatoria();
+    // console.log(tabuleiroCompleto[0]);
+    // atribuirCasaInicial();
 }
  
 function escolherCasaAleatoria(){
-    let linha = parseInt ((Math.random() * (altura - 1)));
-    let coluna = parseInt ((Math.random() * (largura - 1)));
-    let idCasa = gerarId(linha, coluna);
-    return document.getElementById(idCasa);
-    // let playerCasa = document.getElementById(idCasa);
-    
-
+    let i = parseInt ((Math.random() * (tabuleiroCompleto.length - 1)));
+    return tabuleiroCompleto[i];
 }
 
 function atribuirCasaInicial(){
-    playerCasa = escolherCasaAleatoria();
-    swapCasaClass(playerCasa, vazia, personagem);
-    Jogador.construtor(playerCasa, linha, coluna);
+    let houseNow = escolherCasaAleatoria();
+    swapCasaClass(houseNow.casa, vazia, personagem);
+    Jogador.construtor(houseNow);
+    console.log(Jogador);
+   
 }
 
 function swapCasaClass(casa, remClass, addClass){
@@ -122,13 +135,111 @@ document.addEventListener("keydown", function(e){
 
 
 function gerarLabirinto(){
+    let casaInicial = escolherCasaAleatoria();
+    // let casaDois = escolherCasaAleatoria();
+    swapCasaClass(casaInicial.casa, vazia, aberto);
+    pilha.push(casaInicial);
+    // pilha.push(casaDois);
 
+    // while(!pilha.empty){
+        pegarFuraveis(pilha.slice(-1));
+
+    // }
+    
+    
+    
+    // console.log(pilha.slice(-1));
 }
 
 
+function pegarFuraveis(casa){
+    console.log(casa);
+    let vizinhos = getNeibs(casa[0]);
+    // console.log(vizinhos);
+    for(viz of vizinhos){
+        let fur = furavel(viz);
+        if(!fur){
+            vizinhos.splice(1,1,viz);
+        }
+    }
+    
+    console.log(vizinhos);
+}
 
+function furavel(vizinho){
+    let cont = 0;
+    
+    if(!vizinho.casa.classList.contains(vazia)){
+        return false;
+    }
+    let vizinhosSegCamada = getNeibs(vizinho);
+    
+    
+    for(vizinhoSeg of vizinhosSegCamada){
+        if(vizinhoSeg.casa.classList.contains(vazia)){
+            cont++;
+        }
+    }
 
+    if(cont >= 3){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 
+function getNeibs(casa){
+    let vizinhos = [];
+    
+    if(casa.x - 1 >= 0 && casa.y < largura){
+        casaAcima = new House (
+            document.getElementById(gerarId(casa.x - 1, casa.y)),
+            casa.x - 1,
+            casa.y
+        );
+        vizinhos.push(
+            casaAcima
+        );
+    }
+
+    if(casa.x + 1 < altura && casa.y < largura){
+        vizinhos.push(
+            new House (
+                document.getElementById(gerarId(casa.x + 1, casa.y)),
+                casa.x + 1,
+                casa.y
+            )
+        );
+    }
+
+    if(casa.x < altura && casa.y - 1 >= 0){
+        vizinhos.push(
+            new House (
+                document.getElementById(gerarId(casa.x, casa.y - 1)),
+                casa.x,
+                casa.y - 1
+            )
+        );
+    }
+
+    if(casa.x < altura && casa.y + 1 < largura){
+        vizinhos.push(
+            new House (
+                document.getElementById(gerarId(casa.x, casa.y + 1)),
+                casa.x,
+                casa.y + 1
+            )
+        );
+    }
+    for(viz of vizinhos){
+        if(viz == null){
+            vizinhos.splice(1,1,viz);
+        }
+    }
+
+    return vizinhos;
+}
 
 
 
